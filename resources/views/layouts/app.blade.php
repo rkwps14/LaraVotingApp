d0acd67435e03287b295b7d9d9cd53bd649fd83d
rkwps14 <rkwps14@gmail.com>
2021-12-08 17:16:39 +0530

    Episode 4

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,7 +50,40 @@
     </header>
     <main class="container mx-auto flex">
         <div style="max-width:280px; margin-right:20px">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, distinctio in nisi aspernatur veniam vitae veritatis sapiente laborum beatae, architecto, assumenda esse vero consectetur quidem voluptatum. Ipsum distinctio magni nihil, explicabo minus minima expedita esse? Dolore eligendi dolores, ab tempora suscipit earum inventore? Rem incidunt fugit quia eaque iure debitis qui odit iusto, dolorem facere eius unde, atque error. Sit commodi libero nihil, earum eos iste consequatur corporis dolorem nesciunt qui recusandae enim eius mollitia deserunt molestias eum molestiae sapiente necessitatibus possimus laborum sint. Officiis quae impedit non? Provident voluptatum repellat, rerum magnam ipsam quasi illum reprehenderit odio amet tenetur?
+            <div class="bg-white border-2 border-blue rounded-xl mt-16">
+                <div class="text-center px-6 py-2 pt-6">
+                    <h3 class="font-semibold text-base">Add an idea</h3>
+                    <p class="text-xs mt-4">Let us know what you would like and we'll take a look over!</p>
+                </div>
+                <form action="#" method="post" class="space-y-4 px-4 py-6">
+                    <div>
+                        <input type="text" class="text-sm  w-full bg-gray-100 rounded-xl px-4 py-2 border-none" placeholder = "Your Idea">
+                    </div>
+                    <div>
+                        <select name="category_add" id="category_add" class="text-sm  w-full bg-gray-100 rounded-xl px-4 py-2 border-none">
+                                <option value="Category One"> Category One</option>
+                                <option value="Category One"> Category One</option>
+                                <option value="Category One"> Category One</option>
+                                <option value="Category One"> Category One</option>
+                                <option value="Category One"> Category One</option>
+                        </select>
+                    </div>
+                    <div>
+                        <textarea name="idea" id="idea" cols="30" rows="4" class="w-full bg-gray-100 rounded-xl placeholder-gray-900 text-sm px-4 py-2 border-none" placeholder="Describe your idea"></textarea>
+                    </div>
+                    <div class="flex item-center justify-between space-x-3">
+                        <button type="button" class="flex item-center justify-center w-1/2 h-11 text-us bg-gray-200 font-semibold rounded-xl border border-gray-200 hover:border-gray-400 transition duration-150 ease-in">
+                            <svg class="mt-3 text-gray-500 w-4 transform -rotate-45" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13" />
+                            </svg>    
+                            <span class="ml-1 mt-2">Attach</span>
+                        </button>
+                        <button type="submit" class="flex item-center justify-center w-1/2 h-11 text-us bg-blue font-semibold rounded-xl border border-blue hover:border-blue-400 transition duration-150 ease-in">
+                            <span class="ml-1 mt-2">Submit</span>
+                        </button>
+                    </div>
+                </form>
+            </div>
         </div>
         <div style="max-width:700px">
             <nav class="flex item-center justify-between text-xs">
